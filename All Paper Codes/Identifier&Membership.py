1052666245424cc9f2812636b41cb26b1d0c1d69
Rodney Dabre <128955508+dabrerodney@users.noreply.github.com>
2024-05-16 06:42:44 +0530

    done

--- All Paper Codes/Identifier&Membership.py
+++ All Paper Codes/Identifier&Membership.py
@@ -1,15 +1,21 @@
-color = ["red", "yellow", "green"]
+# color = ["red", "yellow", "green"]
 
-t1 = (10, 20, 30)
-t2 = (10, 20, 30)
-t3 = t1
+# t1 = (10, 20, 30)
+# t2 = (10, 20, 30)
+# t3 = t1
 
-print("Membership operator Eg")
-print("yellow" in color)
-print("black" not in color)
-print("brown" in color)
+# print("Membership operator Eg")
+# print("yellow" in color)
+# print("black" not in color)
+# print("brown" in color)
 
-print("Identity operator Eg")
-print(t1 is t2)
-print(t1 is t3)
-print(t3 is t2)
+# print("Identity operator Eg")
+# print(t1 is t2)
+# print(t1 is t3)
+# print(t2 is not t3)
+
+car = 'racecar'
+print(car[:3])
+print(car[2])
+print(car[6])
+print(car[:])
--- All Paper Codes/palindroma.py
+++ All Paper Codes/palindroma.py
@@ -1,10 +1,28 @@
-def checkString(str):
-    return str[::-1]
+# def checkString(str):
+#     return str[::-1]
 
-str = input("Enter the string: ")
-new_str = checkString(str)
+# str = input("Enter the string: ")
+# new_str = checkString(str)
 
-if str == new_str:
-    print(f"{str} is a palindrome")
-else:
-    print(f"{str} ia not a palindrom")
+# if str == new_str:
+#     print(f"{str} is a palindrome")
+# else:
+#     print(f"{str} ia not a palindrom")
+
+# l1 = [23, 99, 56, 72, 57, 88]
+# print("Before sorting", l1)
+# l1.sort()
+# print("After Sorting", l1)
+
+def sum_of_digits(number):
+    digit_sum = 0
+    while number > 0:
+        digit = number % 10
+        digit_sum += digit
+        number //= 10
+    return digit_sum
+
+number = 12345
+print("Sum of digits in", number, "is:", sum_of_digits(number))
+
+
